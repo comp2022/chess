417131b2bb5d86f2f9169bb74b33aac103a6eef7
Mitchell Merry <mitchellmerry145@gmail.com>
2022-02-21 23:52:24 +1100

    Add ability to load states in from .json files.

--- src/api/Chess.ts
+++ src/api/Chess.ts
@@ -111,3 +111,20 @@ function getKingMoves(board: Board, coord: Coordinate): Coordinate[] {
     let moves: Coordinate[] = [];
     return moves;
 }
+
+export function convertRawToBoard(raw: number[][]): Board {
+    return raw.map(row => row.map(convertNumToPiece));
+}
+
+const numToPieceType: PieceType[] = [ 'pawn', 'rook', 'knight', 'bishop', 'queen', 'king' ];
+
+function convertNumToPiece(num: number): (Piece | null) {
+    if(num === 0) return null;
+
+    const piece: Piece = {
+        isBlack: (num & 0x8) === 8,
+        type: numToPieceType[(num & 0x7)-1]
+    }
+
+    return piece;
+}
