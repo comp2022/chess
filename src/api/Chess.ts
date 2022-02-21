95afda81ccd7c6d9efa2cddd01113622bf029bb5
Mitchell Merry <mitchellmerry145@gmail.com>
2022-02-21 17:58:46 +1100

    Complete getKnightMoves, add coordOOB to Chess API

--- src/api/Chess.ts
+++ src/api/Chess.ts
@@ -30,6 +30,10 @@ export function getValidMoves(board: Board, coord: Coordinate): Coordinate[] {
     return moveGenerators[currentPiece.type](board, coord);
 }
 
+export function coordOOB(coord: Coordinate): boolean {
+    return coord.row < 0 || coord.col < 0 || coord.row > 7 || coord.col > 7;
+}
+
 function getPawnMoves(board: Board, coord: Coordinate): Coordinate[] {
     let moves: Coordinate[] = [];
     return moves;
@@ -42,8 +46,21 @@ function getBishopMoves(board: Board, coord: Coordinate): Coordinate[] {
 
 function getKnightMoves(board: Board, coord: Coordinate): Coordinate[] {
     let moves: Coordinate[] = [];
-    return moves;
+    
+    const knightMove = [1, 2]; // used to generate all other moves
+    for(let i = 0; i < 8; i++) {
+        moves.push({
+            row: coord.row + knightMove[i < 4 ? 0 : 1] * ((i & 0x1) === 1 ? -1 : 1),
+            col: coord.col + knightMove[i < 4 ? 1 : 0] * ((i & 0x2) === 2 ? -1 : 1),
+        });
+    }
+
+    return moves.filter(move => 
+        !coordOOB(move) && // the move is inbounds
+        board[move.row][move.col]?.isBlack !== board[coord.row][coord.col]?.isBlack // the cell does not have a piece of the same colour
+    );
 }
+
 function getRookMoves(board: Board, coord: Coordinate): Coordinate[] {
     let moves: Coordinate[] = [];
     return moves;
