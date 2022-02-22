ddd916ab54c41e3534abce13d6bd8dc961010a97
Mitchell Merry <mitchellmerry145@gmail.com>
2022-02-22 21:51:14 +1100

    Revert "Add basis of an API (#2)"
    This reverts commit 31423f8cf3fdd2ba0a10fddee88fbcd2e961c6d5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import './App.scss';
-import { BoardComponent } from './components/board/BoardComponent';
-import state from './api/boardstates/default.json';
-import { Board, convertRawToBoard } from './api/Chess';
+import './App.css';
 
 function App() {
-  const initialBoard: Board = convertRawToBoard(state);
-
-  return <div id="app">
-    <BoardComponent board={initialBoard}/>
-  </div>;
+  return (
+    <></>
+  );
 }
 
 export default App;
