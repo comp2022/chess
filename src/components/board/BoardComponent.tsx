85d5510ce6c53b60c5d46edc4e333ff056106f4d
Alvin <alvinmaung@gmail.com>
2022-02-24 22:35:33 +1100

    Allow chess pieces to move on their turn

--- src/components/board/BoardComponent.tsx
+++ src/components/board/BoardComponent.tsx
@@ -25,9 +25,36 @@ export interface BoardProps {
 // FC stands for Functional Component. This function returns a Functional Components with the props of BoardProps
 export const BoardComponent: React.FC<BoardProps> = ({ board }) => {    
     const [ currentBoard, setCurrentBoard ] = useState<Board>(board);
+    const [ previousMove, setpreviousMove ] = useState<Coordinate>();
+    const [ invokeMove, setInvokeMove ] = useState<Boolean>(false);
+    const [ isBlackTurn, setIsBlackTurn ] = useState<Boolean>(false);
     const [ possibleMoves, setPossibleMoves ] = useState<Coordinate[]>([]);
 
-    useEffect(() => console.log(possibleMoves), [possibleMoves]);
+    const updateBoard = (currentMove: Coordinate): void => {
+        // if the selected move is valid
+        if (previousMove && possibleMoves.some(({row: pRow, col: pCol}) => pRow === currentMove.row && pCol === currentMove.col)) {
+            currentBoard[currentMove.row][currentMove.col] = currentBoard[previousMove.row][previousMove.col];
+            currentBoard[previousMove.row][previousMove.col] = null;
+            setCurrentBoard(currentBoard);
+            setPossibleMoves([]);
+            setIsBlackTurn(!isBlackTurn)
+            setInvokeMove(false);
+        } 
+    }
+
+    // invoke move if we select the chess piece
+    const invokeMoves = (move: Coordinate): void => {
+        (getValidMoves(currentBoard, move).length) ? setInvokeMove(true) : setInvokeMove(false);
+        // store previousMoveious move
+        setpreviousMove(move);
+    }
+
+    const blackTurn = (move: Coordinate): boolean => {
+        // return true if black turn
+        return (currentBoard[move.row][move.col] !== null && currentBoard[move.row][move.col]?.isBlack === true);
+    }
+
+    useEffect(() => console.log(possibleMoves, "dfs"), [possibleMoves]);
 
     return <div className={styles.board}>
    
@@ -37,14 +64,20 @@ export const BoardComponent: React.FC<BoardProps> = ({ board }) => {
                 <span className={styles.cellTag}>{rowIndex}</span>
                 { row.map((piece, colIndex) => {
                     const highlighted = possibleMoves.some(({row: pRow, col: pCol}) => pRow === rowIndex && pCol === colIndex);
+                    
                     return <Cell
                         isBlack={(rowIndex + colIndex) % 2 === 1}
                         highlighted={highlighted}
                         key={`${rowIndex} ${colIndex}`}
                         piece={piece} 
                         onClick={() => {
-                            setPossibleMoves(getValidMoves(currentBoard, { row: rowIndex, col: colIndex }));
-                            console.log(rowIndex, colIndex)
+                            if (isBlackTurn === blackTurn({ row: rowIndex, col: colIndex })){
+                                invokeMoves({ row: rowIndex, col: colIndex })
+                                setPossibleMoves(getValidMoves(currentBoard, { row: rowIndex, col: colIndex }))
+                            }
+                            if (invokeMove) {
+                                updateBoard({ row: rowIndex, col: colIndex });
+                            } 
                         }}
                     />;
                 })}
