07d95437bc78bb65ad3b1f3c847767139320b82f
Mitchell Merry <mitchellmerry145@gmail.com>
2022-02-27 16:42:15 +1100

    Implement first version of convertFENToBoard

--- src/api/load_board.ts
+++ src/api/load_board.ts
@@ -1,4 +1,4 @@
-import { Board, Piece, PieceType } from ".";
+import { Board, FEN, Piece, PieceType } from ".";
 
 export function convertRawToBoard(raw: number[][]): Board {
     return raw.map(row => row.map(convertNumToPiece));
@@ -16,4 +16,50 @@ function convertNumToPiece(num: number): (Piece | null) {
     }
 
     return piece;
+}
+
+const sanToPieceType: Record<string, PieceType> = {
+    'p': 'pawn',
+    'n': 'knight',
+    'b': 'bishop',
+    'r': 'rook',
+    'q': 'queen',
+    'k': 'king'
+};
+
+// https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation#Definition
+function convertFENToBoard(fen: FEN): Board {
+    const [boardState, turn, castleState, epSquare, halfmoveClock, fullMoveNumber] = fen.split(" ");
+
+    const board = boardState.split("/").map((rankState, rankIndex) => {
+        const rank: (Piece | null)[] = [...Array(8)].map(_ => null); // null by default
+
+        let col = 0;
+        // standard algebraic notation, not actually SAN but best-fitting
+        for(let san of rankState.split("")) {
+            if(col > 8) throw new Error(`Invalid FEN: Too many cells described for rank ${rankIndex}.`);
+
+            // check if its describing an amount of empty places
+            const val = Number.parseInt(san);
+            if(isNaN(val)) {
+                col += val; // advance by the specified number of cells
+                continue;
+            }
+
+            // otherwise it describes a piece
+
+            // uppercase means white, lowercase means black
+            const color = san === san.toUpperCase() ? 'white' : 'black';
+            
+            // force lowercase to check piece type
+            san = san.toLowerCase();
+            const type = sanToPieceType[san];
+
+            rank[col] = { color, type };
+        }
+
+        return rank;
+    });
+
+    return board;
 }
