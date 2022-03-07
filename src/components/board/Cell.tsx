ba229e9bd28627b0a5cbaaa2d20afd2301ad457d
Alvin <alvinmaung@gmail.com>
2022-03-07 23:27:38 +1100

    Remove invalid cursor and center pointer to drag preview image

--- src/components/board/Cell.tsx
+++ src/components/board/Cell.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Coordinate, Piece } from "../../api";
 import styles from './Board.module.scss';
 import { chesspieces } from "../../assets/chesspieces";
 import { useDrag, useDrop } from 'react-dnd'; //https://react-dnd.github.io/react-dnd/docs/overview
 const classNames = require('classnames'); // https://github.com/JedWatson/classnames
+// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 export interface CellProps {
     isBackgroundBlack: boolean;
@@ -21,11 +22,10 @@ export interface CellProps {
 
 export const Cell: React.FC<CellProps> = ({ isBackgroundBlack, piece, onClick, moveHint, isHighlighted, rankHint, fileHint, coord, onMouseDown, childSetDropCoord, possibleMoves }) => {
 
-    // set drag
-    const [{ isOver, canDrop }, drop] = useDrop({
+    // // set drag
+    const [{ isOver }, drop] = useDrop({
         accept: 'piece',
         // check for valid / invalid moves
-        canDrop: () => (possibleMoves.some(( [pRow, pCol] ) => pRow === coord[0] && pCol === coord[1])),
         drop: () => childSetDropCoord(coord), // invoked when the piece is dropped
         collect: (monitor) => ({
             isOver: !!monitor.isOver(),
@@ -34,7 +34,7 @@ export const Cell: React.FC<CellProps> = ({ isBackgroundBlack, piece, onClick, m
     });
 
     // set drop
-    const [{ isDragging }, drag] = useDrag(() => ({
+    const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
 		// "type" is required. It is used by the "accept" specification of drop targets.
         type: 'piece',
 
@@ -43,7 +43,11 @@ export const Cell: React.FC<CellProps> = ({ isBackgroundBlack, piece, onClick, m
         })
     }));
 
-    let pieceImg;
+    const canDrop = () => {
+        return possibleMoves.some(( [pRow, pCol] ) => pRow === coord[0] && pCol === coord[1]);
+    }
+
+    let pieceImg: any;
     if (piece) {
         const index = (piece.color === 'white') ? 0 : 1;
         pieceImg = chesspieces[piece.type][index];
@@ -52,8 +56,9 @@ export const Cell: React.FC<CellProps> = ({ isBackgroundBlack, piece, onClick, m
     let cn = classNames(
         [ styles.cell ], 
         { [ styles.cellOdd ]: isBackgroundBlack }, // determines shade of cell
-        { [ styles.cellValid]: canDrop && isOver }, // borderValid
-        { [ styles.cellInvalid]: !canDrop && isOver }, // borderInvalid
+        { [ styles.cellValid]: canDrop() && isOver }, // borderValid
+        { [ styles.cellInvalid]: !canDrop() && isOver }, // borderInvalid
+        { [ styles.dragging ]: isDragging },
     );
     
     const rankHintClass = classNames( 
@@ -68,28 +73,51 @@ export const Cell: React.FC<CellProps> = ({ isBackgroundBlack, piece, onClick, m
         { [ styles.coordHintOdd ]: isBackgroundBlack },
     );
 
-    
+    useEffect(() => {
+        const img = new Image();
+        const newImg = new Image();
+        // calculate the preview image size according to view width
+        const size = (Math.min(window.innerWidth, window.innerHeight) / 8) * 0.8;
+        
+        if (pieceImg) {
+            img.src = pieceImg;
+            const ctx = document.createElement('canvas').getContext('2d');
+            if (ctx) {
+                ctx.canvas.width = size;
+                ctx.canvas.height = size;
+                // create a preview image to be the same as the original piece image
+                img.onload = function() {
+                    ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+                    newImg.src = ctx.canvas.toDataURL();
+                };
+            }
+            // center the cursor on the image
+            const previewOptions = {
+                offsetX: (size / 2) + (size * 0.1),
+                offsetY: (size / 2)
+            };
+
+            dragPreview(newImg, previewOptions);
+        }
+            
+    },[isDragging, window.innerWidth, window.innerHeight])
+
     return (
         // use onMouseDown to setActiveCell when dragging
         <div className={cn} onClick={() => onClick()} onMouseDown={() => onMouseDown()}  ref={drop}>
         {/* highlighted, goes under the piece image */}
         { isHighlighted && <div className={styles.highlighted} />}
 
-        {/* Highlight cell when we drag chess piece on it */}
-        {/* {isOver && !canDrop && <div className={styles.highlightedInvalidCell } />}
-        {isOver && canDrop && <div className={styles.highlightedValidCell } />} */}
-
         {/* rank / file hints */}
         {rankHint && <span className={rankHintClass}>{rankHint}</span>}
         {fileHint && <span className={fileHintClass}>{fileHint}</span>}
 
         {/* piece image */}
-        { piece && <img src={pieceImg} ref={drag} alt={ piece.type } className={styles.img} style={{ opacity: isDragging ? 0 : 1 }} />}
+        { piece && <img src={pieceImg} ref={drag}  alt={ piece.type } className={styles.img} style={{ opacity : isDragging ? 0 : 1}} />}
 
         {/* hint circle, goes over the piece image */}
         { moveHint && <div className={styles.moveHint} />}
         </div>
     )
-        
-   
+
 }
